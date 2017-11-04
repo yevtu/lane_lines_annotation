4c883cc5e15381627fc47e1e6e0dbd10b99e82c6
SergYevtukhoff <serg.yevtukhoff@yandex.ru>
2017-11-05 00:45:59 +0200

    Process every second frame

--- annotator.py
+++ annotator.py
@@ -6,12 +6,14 @@ from image_handler import *
 
 class Annotator:
     def __init__(self, in_path, out_path):
+        self._frame_n = 0
         self._in_path = in_path
         self._out_path = out_path
-        self._prev_lines = deque([], 10)
+        self._prev_lines_params = deque([], 10)
     
     def process_image(self, image):
         result = self.detect_lane_lines(image)
+        self._frame_n += 1
         return result
 
     def process_video(self):
@@ -20,10 +22,10 @@ class Annotator:
         result.write_videofile(self._out_path, audio=False)
 
     def get_mean_params(self):
-        if not len(self._prev_lines):
+        if not len(self._prev_lines_params):
             return None
 
-        params = pd.DataFrame(list(self._prev_lines))
+        params = pd.DataFrame(list(self._prev_lines_params))
         means = {}
         for col in params.columns:
             means[col] = np.nanmean(params[col].values)
@@ -32,7 +34,7 @@ class Annotator:
     def detect_lane_lines(self, image):
         x_size, y_size = image.shape[1], image.shape[0]
 
-        blur_gray = gaussian_blur(grayscale(image), 3)
+        blur_gray = gaussian_blur(grayscale(image), kernel_size=3)
         canny_image = canny(blur_gray, low_threshold=10, high_threshold=200)
 
         left_bottom = (int(0.2 * x_size), int(0.9 * y_size))
@@ -42,15 +44,16 @@ class Annotator:
         vertices = np.array([[left_bottom, left_upper, right_upper, right_bottom]], dtype=np.int32)
         masked_image = region_of_interest(canny_image, vertices)
 
-        lines, params = hough_lines(masked_image,
-                                    self.get_mean_params(),
-                                    rho=1,
-                                    theta=np.pi / 180,
-                                    threshold=30,
-                                    min_line_len=100,
-                                    max_line_gap=120,
-                                    slope_threshold=0.55)
-        self._prev_lines.append(params)
+        if not self._frame_n % 2:
+            self._lines, params = hough_lines(masked_image,
+                                        self.get_mean_params(),
+                                        rho=1,
+                                        theta=np.pi / 180,
+                                        threshold=30,
+                                        min_line_len=100,
+                                        max_line_gap=120,
+                                        slope_threshold=0.55)
+            self._prev_lines_params.append(params)
 
-        result = weighted_img(lines, image)
+        result = weighted_img(self._lines, image)
         return result
